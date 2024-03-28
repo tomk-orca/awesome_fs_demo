2339fbbfa3cce88f29d481b82b53b13f5dc819c8
Tom Knobel <tom.knobel@orca.security>
2024-03-28 14:49:59 +0200

    test 2

--- error.go
+++ error.go
@@ -10,7 +10,7 @@ type OrcaError struct {
 
 func (err OrcaError) Error() string {
 
-	return err.Err.Error() + "\n this is orca error file 2"
+	return err.Err.Error() + "\n this is orca error file 3"
 }
 
 func New(e error) OrcaError {
