76beaad193238894e746b0026aef2697fc29446d
tomk-orca <104308480+tomk-orca@users.noreply.github.com>
2024-03-28 11:10:19 +0200

    Add files via upload
    FIRST COMMIT

---
+++ error.go
@@ -0,0 +1,21 @@
+package orcaError
+
+import (
+	"errors"
+)
+
+type OrcaError struct {
+	Err error `json:"-"`
+}
+
+func (err OrcaError) Error() string {
+
+	return err.Err.Error() + "\n this is orca error"
+}
+
+func New(e error) OrcaError {
+	e.Error = 
+	return OrcaError{
+		Err: e,
+	}
+}
---
+++ main.py
@@ -0,0 +1,17 @@
+
+def show_secret():
+	# juest  a test
+	# test 
+	# orca-secrets ignore-line
+	GITHUB_PERSONAL_TOKEN = "ghp_ZRrWptP6rQQq0pSuiSh15HzmAdrIVV2zkXNp"
+	# orca-secrets ignore-line
+	GITHUB2_PERSONAL2_TOKEN22 = "ghp_ZRrWptTOSEEP6rQQq0pSuiSh15HzmAdrIVV2zkXNp"
+	GITHUB3_PERSONAL3_TOKEN33 = "ghp_ZRrWptTOSE3P6rQQq0pSuiSh15HzmAdrIVV2zkXNp"
+	print(f"GITHUB_PERSONAL_TOKEN")
+	print(GITHUB_PERSONAL_TOKEN)
+    GITHUB3_PERSONAL4_TOKEN44 = "ghp_ZRrWptTOSE4P6rQQq0pSuiSh15HzmAdrIVV2zkXNp"
+
+
+if __name__ == "__main__":
+	show_secret()
+
