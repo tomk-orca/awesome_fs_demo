5c2b338be9cfde5c04ac79d59535706f85dbae98
tomk-orca <104308480+tomk-orca@users.noreply.github.com>
2024-03-28 11:21:37 +0200

    test (#3)

--- error.go
+++ error.go
@@ -10,7 +10,7 @@ type OrcaError struct {
 
 func (err OrcaError) Error() string {
 
-	return err.Err.Error() + "\n this is orca error file"
+	return err.Err.Error() + "\n this is orca error file 2"
 }
 
 func New(e error) OrcaError {
