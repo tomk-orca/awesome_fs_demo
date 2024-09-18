57e81e464acfab1121796509867ee31fb9398fd6
tomk-orca <104308480+tomk-orca@users.noreply.github.com>
2024-09-18 15:59:42 +0300

    dccdd

--- error.go
+++ error.go
@@ -10,7 +10,7 @@ type OrcaError struct {
 
 func (err OrcaError) Error() string {
 
-	return err.Err.Error() + "\n this is orca11 error111 file 2"
+	return err.Err.Error() + "\n this is orca11wwdsq1 error111 file 2"
 }
 
 func New(e error) OrcaError {
