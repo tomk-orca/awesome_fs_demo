5300b66ffa1277ff1e26f0e93e2337f3f8cf32e9
tomk-orca <104308480+tomk-orca@users.noreply.github.com>
2024-09-22 14:01:51 +0300

    vdfvdf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // orca-secrets ignore-line
 GITHUB_PERSONAL_TOKEN=ghp_ZRrWptP6rt
 GITHUB_PERSONAL_TO_NOT_IGNORE_TOKEN=ghp_ZRrWptTOSEEP6rQQq0pSuiSh15HzmAdrIVV2zkXNp
-console.log(`i have vuknrabilty in my pakge and secret in my files ${GITHUB_PERSONAL_TO_NOT_IGNORE_TOKEN}`)
+console.log(`i have vuknracccbilty in my pakge and secret in my files ${GITHUB_PERSONAL_TO_NOT_IGNORE_TOKEN}`)
