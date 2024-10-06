28e63a3923c4a3728d54fa0a3ec632100cb2a4e6
tomk-orca <104308480+tomk-orca@users.noreply.github.com>
2024-10-06 16:10:43 +0300

    wdxwqd

--- error.go
+++ error.go
@@ -10,7 +10,7 @@ type OrcaError struct {
 
 func (err OrcaError) Error() string {
 
-			return err.Err.Error() + "\n ths ssss ewssswdw ssss file 2"
+			return err.Err.Error() + "\n ths sss111s ewssswdw ssss file 2"
 }
 func New(e error) OrcaError {
 	e.Error = 
